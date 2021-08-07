6802630986c8f705afd374f4f60d39d6af142eb1
Myles Williams <83609054+MylesFW@users.noreply.github.com>
2021-08-06 17:18:47 -0700

    file restructure 1.0

---
+++ portfolio/ProjectName/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Home
+{
+    public class HomeController : Controller
+    {
+
+      [HttpGet("")]
+      public ViewResult Index()
+      {
+        return View();
+      }
+
+
+      [HttpGet("contact")]
+      public ViewResult Contact()
+      {
+        return View();
+      }
+
+
+      [HttpGet("project")]
+      public ViewResult Projects() //must have the same name as the url
+      {
+        return View();
+      }
+
+      [HttpGet("example")]
+      public RedirectToActionResult Example()
+      {
+        return RedirectToAction("");
+      }
+
+    }
+}
