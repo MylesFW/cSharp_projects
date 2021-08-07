4b8de9587b1f84a16b09cd1caa271782ec239a38
Myles Williams <83609054+MylesFW@users.noreply.github.com>
2021-08-07 13:39:02 -0700

    inti commit / file name change

---
+++ viewmodel_fun_rand_gen/Models/User.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace survey_with_validation.Models
+{
+    public class User
+    {
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+    }
+}
