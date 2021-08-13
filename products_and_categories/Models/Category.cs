4d92ae3c7db9ae686d8d8c5d1f4d766da0cd101c
Myles Williams <83609054+MylesFW@users.noreply.github.com>
2021-08-13 14:02:23 -0700

    prod_and_ctg init commit

---
+++ products_and_categories/Models/Category.cs
@@ -0,0 +1,7 @@
+namespace products_and_categories.Models
+{
+    public class Category
+    {
+        
+    }
+}
---
+++ products_and_categories/Models/Product.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace products_and_categories.Models
+{
+    public class Product
+    {
+        [Key]
+        [Required]
+        public int ProductId {get;set;}
+        public string ProductName {get;set;}
+        public string ProductDescription {get;set;}
+        public double ProductPrice {get;set;}
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public List<Association> Association {get;set;}
+        }
+}
